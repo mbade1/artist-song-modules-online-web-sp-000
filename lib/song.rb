03f4aba18b3207f604054afb5a2fb9cfe4bf43b9
Michael Bade <mbade314@gmail.com>
2020-05-22 16:07:20 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,26 +1,22 @@
-require 'pry'
-
 class Song
+
+  extend Memorable::ClassMethods
+  extend Findable::ClassMethods
+  include Memorable::InstanceMethods
+  include Paramable::InstanceMethods
+
   attr_accessor :name
   attr_reader :artist
 
-  extend Memorable
-  include Paramable
-
-
-  @@all = []
-
-  def initialize
-    @@all << self
-  end
+  @@songs = []
 
   def self.all
-    @@all
+    @@songs
   end
 
   def artist=(artist)
     @artist = artist
+    artist.add_song(self) unless artist.songs.include?(self)
   end
-
-
+  
 end
